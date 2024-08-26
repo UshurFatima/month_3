19bfc3c185cfef27ff1ac7239cdb8a74052d1950
UshurFatima <ushurfatima@gmail.com>
2024-08-26 12:46:36 +0600

    parsing

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,12 +5,14 @@ from .echo import echo_router
 from .start import start_router
 from .book_survey import survey_router
 from .picture import picture_router
+from .mashina_kg import mashina_router
 from .group_activities import group_router
 
 
 private_router = Router()
 
 private_router.include_routers(start_router, survey_router,
-                               shop_router, picture_router, echo_router)
+                               shop_router, picture_router, mashina_router,
+                               echo_router)
 
 private_router.message.filter(F.chat.type == 'private')
